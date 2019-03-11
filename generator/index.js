f4ba38b6eec7b1f191963c1fd331999cc4a691d2
youngdze <youngdze94@gmail.com>
2019-03-11 22:57:53 +0800

    remove dependencies

--- generator/index.js
+++ generator/index.js
@@ -7,10 +7,6 @@ module.exports = (api, options) => {
         dependencies: {
             "vue-router": "^3.0.1",
             "vuex": "^3.0.1"
-        },
-        devDependencies: {
-            "mpvue-webpack-target": "^0.0.1",
-            "string-replace-loader": "^2.1.1",
         }
     }
     api.extendPackage(pkg);
